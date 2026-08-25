868775cef87e161dcd23e9180aaba820a04bb41f
itianyu <itianyu@home.com>
2026-08-25 16:25:44 +0800

    1. GOGOC原理 清除终止-并发标记-标记终止-并发清除 循环

--- interfaceee/runtime.go
+++ interfaceee/runtime.go
@@ -1,8 +1,11 @@
 package interfaceee
 
 import (
+	"context"
 	"fmt"
+	"net/http"
 	"runtime/metrics"
+	"time"
 )
 
 // GC调优
@@ -35,4 +38,83 @@ func readGC() {
 	for _, s := range samples {
 		fmt.Println(s.Name, s.Value)
 	}
+}
+
+// go内存泄漏：分配的内存不能被GC,导致内存持续增长。pprof heap持续增长 + goroutine profile 协程泄漏
+// go内存逃逸：分配的内存超过了自身的作用域，栈上的空间被分配到堆上。导致GC压力，cpu吞吐降低；go build -gcflag="m" [program]
+// 生产环境：
+// Prometheus + Grafana + Pyroscope
+// 网络轮询器epoll和kqueue实现原理
+// gc初始化、开始、标记、清理、结束
+// 内存泄漏
+var (
+	oom1Slice = make([][]int, 0)
+)
+
+// oom1 无限写入
+func oom1() {
+	tmp := make([]int, 0)
+	for {
+		tmp = append(tmp, 1)
+		oom1Slice = append(oom1Slice, tmp)
+		time.Sleep(time.Microsecond * 20)
+	}
+}
+
+// oom2 http响应body未关闭
+func oom2() {
+	rsp, _ := http.Get("https://www.google.com")
+
+	//defer rsp.Body.Close()
+
+	fmt.Println(rsp.StatusCode)
+}
+
+// 协程泄漏
+// oom3 goroutine阻塞导致协程泄漏(连带内存泄漏)
+func oom3() {
+	ch := make(chan struct{})
+	go func() {
+		ch <- struct{}{}
+	}()
+}
+
+// oom3Fix 使用带缓冲的channel 或 context限制超时时间 或 有人接收
+func oom3Fix(ctx context.Context) {
+	ch := make(chan struct{}, 1)
+	go func() {
+		select {
+		case ch <- struct{}{}:
+		case <-ctx.Done():
+		}
+	}()
+	<-ch
+}
+
+// oom4 slice引用底层大数组，内存不能被GC释放
+func oom4() []byte {
+	big := make([]byte, 1024*1024*100)
+
+	small := big[:10]
+	small[0] = 1
+
+	fmt.Println(big[0], big[1], big[2], big[3])
+
+	return small
+}
+
+// 内存逃逸
+
+// escape1 返回局部变量的指针
+func escape1() *int {
+	x := 1
+	return &x
+}
+
+type es struct {
+	Name string
+}
+
+func escape2() *es {
+	return &es{"name is escape"}
 }
