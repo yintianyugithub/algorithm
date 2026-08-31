9897f2b6b9e9f19e02d8e5ad3e4b2595410d3f3c
itianyu <itianyu@home.com>
2026-08-31 10:27:57 +0800

    1. 缓存预测hashTag解决数据倾斜问题 2. 统计用户登录时长 2.1 合并区间

--- launch.go
+++ launch.go
@@ -1,22 +1,19 @@
 package main
 
 import (
-	"fmt"
-	"runtime/metrics"
+	"context"
+	"interface/interfaceee"
 )
 
 func main() {
-	samples := []metrics.Sample{
-		{Name: "/gc/heap/live:bytes"},        // 上次GC存活的堆内存
-		{Name: "/gc/heap/goal:bytes"},        // 目标GC的堆内存
-		{Name: "/gc/gogc:percent"},           // GC的阈值触发时机
-		{Name: "/gc/cycles/total:gc-cycles"}, // 周期内GC的总次数
-		{Name: "/gc/heap/allocs:bytes"},      // 堆内存分配
-	}
+	ctx := context.TODO()
+	uid := int64(2080)
 
-	metrics.Read(samples)
-
-	for _, s := range samples {
-		fmt.Println(s.Name, s.Value)
-	}
+	interfaceee.UserLogin(ctx, uid)
+	interfaceee.UserLogin(ctx, uid)
+	interfaceee.UserLogin(ctx, uid)
+	interfaceee.UserLogin(ctx, uid)
+	interfaceee.UserLogin(ctx, uid)
+	interfaceee.UserLogin(ctx, uid)
+	interfaceee.CalculateLoginTime(ctx, uid)
 }
